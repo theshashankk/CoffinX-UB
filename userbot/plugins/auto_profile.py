14729305e7a11a81dcc599b9c1b1aa9a6f3d0803
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-05 21:50:47 +0530

    Update auto_profile.py

--- userbot/plugins/auto_profile.py
+++ userbot/plugins/auto_profile.py
@@ -10,7 +10,7 @@ from userbot import ALIVE_NAME, CMD_HELP
 
 BIO_MSG = Config.BIO_MSG
 if BIO_MSG is None:
-  BIO_MSG = "I am a pro @Dark_cobra_support"
+  BIO_MSG = "COFFIN X OP AS ALWAYS 🤖"
 
 DEL_TIME_OUT = 60
 
