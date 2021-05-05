9c1165687d75d981c173d93d8ab0d85a97e4c771
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-05 20:19:46 +0530

    Update __init__.py

--- userbot/plugins/sql_helper/__init__.py
+++ userbot/plugins/sql_helper/__init__.py
@@ -22,3 +22,4 @@ except AttributeError as e:
     # this is a dirty way for the work-around required for #23
     print("DB_URI is not configured. Features depending on the database might have issues.")
     print(str(e))
+#ANDI_MANDI_SANDI_JISNE_KANG_KIYA_USKI_MAA_KII_CHUTT_LALL_KRUUU
