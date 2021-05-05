25220a1a7c144930949b05f40391742e99726433
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-05 20:09:12 +0530

    Update locks_sql.py

--- userbot/plugins/sql_helper/locks_sql.py
+++ userbot/plugins/sql_helper/locks_sql.py
@@ -76,3 +76,5 @@ def get_locks(chat_id):
         return SESSION.query(Locks).get(str(chat_id))
     finally:
         SESSION.close()
+
+        #ANDI_MANDI_SANDI_JISNE_KANG_KIYA_USKI_MAA_KII_CHUTT_LALL_KRUUU
