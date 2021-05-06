2458f126ed8ff660144968a9de4ad68a4009a79d
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-06 12:30:42 +0530

    Update iptools.py

--- userbot/plugins/iptools.py
+++ userbot/plugins/iptools.py
@@ -128,7 +128,7 @@ async def _(event):
         l = result["org"]
         m = result["as"]
         n = result["query"]
-        output = (f"<b><u>Information Gathered Successfully</b></u>\n\n<b>Ip Adress :- </b><code>{n}</code>\n<b>Status :-</b><code>{a}</code>\n<b>Country:- </b> <code>{b}</code>\n<b>Country Code:-</b><code>{c}</code>\n<b>Region :- </b><code>{d}</code>\n<b>Region Name :-</b><code>{e}</code>\n<b>City:- </b> <code>{f}</code>\n<b>Zip :- </b><code>{g}</code>\n<b>Latitude :- </b><code>{h}</code>\n<b>Longitude :- </b><code>{i}</code>\n<b>Timezone :- </b><code>{j}</code>\n<b>ISP :- </b><code>{k}</code>\n<b>ORG :- </b><code>{l}</code>\n<b>AS :- </b><code>{m}</code>\n\n✨ ©DARK COBRA ✨")  
+        output = (f"<b><u>Information Gathered Successfully</b></u>\n\n<b>Ip Adress :- </b><code>{n}</code>\n<b>Status :-</b><code>{a}</code>\n<b>Country:- </b> <code>{b}</code>\n<b>Country Code:-</b><code>{c}</code>\n<b>Region :- </b><code>{d}</code>\n<b>Region Name :-</b><code>{e}</code>\n<b>City:- </b> <code>{f}</code>\n<b>Zip :- </b><code>{g}</code>\n<b>Latitude :- </b><code>{h}</code>\n<b>Longitude :- </b><code>{i}</code>\n<b>Timezone :- </b><code>{j}</code>\n<b>ISP :- </b><code>{k}</code>\n<b>ORG :- </b><code>{l}</code>\n<b>AS :- </b><code>{m}</code>\n\n✨ ©𝙲𝙾𝙵𝙵𝙸𝙽 𝚇 ✨")  
         await cipher.edit(output, parse_mode="HTML")
     except:
         await cipher.edit("Not a Valid ip/host or Don't Have Enough Info.")
