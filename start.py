451fa0b0c95cd3d573b024bb3af46fb044a051f0
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-06 16:01:28 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,8 +3,8 @@ import subprocess
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-os.system("git clone https://github.com/theshashankk/CoffinX-UB coffinX")
-os.chdir("coffinX")
+os.system("git clone https://github.com/theshashankk/CoffinX-UB coffinx-ub")
+os.chdir("coffin-ub")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
