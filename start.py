4bdde848d8d5ec86927748fcc80f40908ce1caeb
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-06 12:13:22 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/theshashankk/CoffinX-UB && cd CoffinX-UB && python3 -m USERBOT")
+system ("git clone https://github.com/theshashankk/CoffinX-UB && cd CoffinX-UB && python3 -m userbot")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m USERBOT")
+system("python3 -m userbot)
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @CoffinXsupport")
